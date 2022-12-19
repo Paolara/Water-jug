8b2099a087d24fd3311c53f631e5ba902daf9ca1
Paola Lara <paolara.0510@gmail.com>
2022-12-20 00:16:01 +0100

    changes

--- Water-jug.js
+++ Water-jug.js
@@ -1,88 +1,72 @@
 const xJugInput = document.querySelector('#x-jug');
 const yJugInput = document.querySelector('#y-jug');
 const zMeasureInput = document.querySelector('#z-measure');
+let button = document.querySelector('#bt');
+button.addEventListener('click', function() {
+    fill(xJugInput.value, yJugInput.value, zMeasureInput.value);
+});
 
-function empty () {
-    
-}
-
-function transfer (jugToEmpty, jug, jugToFill) {
-    let diff = jug - jugToFill;
-    if (diff > jugToEmpty) {
-        jugToFill += Number(jugToEmpty);
-        jugToEmpty = 0;
-    } else {
-        jugToFill += Number(diff);
-        jugToEmpty -= Number(diff);
-    }
-    return jugToEmpty, jugToFill;
-}
-
-function fill () { 
+function fill (X, Y, Z) { 
     let xJug = 0;
     let yJug = 0;
-    let found = false;
+    let found = false
     let count = 0;
-    while (!found) {
-        count++;
-        console.log(found);
-        if (xJug == zMeasureInput.value || yJug == zMeasureInput.value) {
-            found = true;
-            break;
-        }
 
-        if (xJug == 0) {
-            xJug = xJugInput.value;
-            console.log('x', xJug);
-            continue;
-        }
-        
-        if (yJug < yJugInput.value && xJug == xJugInput.value) {
-            let diff = yJugInput.value - yJug;
-            console.log('transfer x', diff);
-            if (diff > xJug) {
-                yJug += Number(xJug);
-                xJug = 0;
-            } else {
-                yJug += Number(diff);
-                xJug -= Number(diff);
-            }
-            console.log('transfer x',xJug,yJug);
-            continue;
-        }
-
-        if (yJug == 0) {
-            yJug = yJugInput.value;
-            console.log('y',yJug);
-            continue;
-        }
-
-        if (xJug < xJugInput.value && yJug == yJugInput.value) {
-            let diff = xJugInput.value - xJug;
-            console.log('transfer y', diff);
-            if (diff > xJug) {
-                xJug += Number(yJug);
-                yJug = 0;
-            } else {
-                xJug += Number(diff);
-                yJug -= Number(diff);
-            }
-            console.log('transfer y',xJug,yJug);
-            continue;
-        }
-
-        if(count == 1000) {
-            found = false;
-            break;
-        }
-    }   
-    if (found) {
-        console.log('solution',xJug, yJug);
+    if (X == Z) {
+        document.querySelector('h4.status').innerHTML = 'The X jug is full with the Z measure';
+    } else if (Y == Z) {
+        document.querySelector('h4.status').innerHTML = 'The Y jug is full with the Z measure';
     } else {
-        console.log('No hay solucion');
+        while (!found) {
+            count++;
+            if (xJug == Z || yJug == Z) {
+                found = true;
+                break;
+            }
+
+            if (xJug == 0) {
+                xJug = X;
+                continue;
+            }
+            
+            if (yJug < Y && xJug == X) {
+                let diff = Y - yJug;
+                if (diff > xJug) {
+                    yJug += Number(xJug);
+                    xJug = 0;
+                } else {
+                    yJug += Number(diff);
+                    xJug -= Number(diff);
+                }
+                continue;
+            }
+
+            if (yJug == 0) {
+                yJug = Y;
+                continue;
+            }
+
+            if (xJug < X && yJug == Y) {
+                let diff = X - xJug;
+                if (diff > xJug) {
+                    xJug += Number(yJug);
+                    yJug = 0;
+                } else {
+                    xJug += Number(diff);
+                    yJug -= Number(diff);
+                }
+                continue;
+            }
+
+            if(count == 100) {
+                found = false;
+                break;
+            }
+        }   
+        if (found) {
+            console.log('solution',xJug, yJug);
+        } else {
+            console.log('No hay solucion');
+        }
     }
-    // if (zMeasureInput.value > (xJugInput.value + yJugInput.value)) {
-    //     document.querySelector('h4.status').innerHTML = 'No Solution';
-    // } else {
-    // }
 }
