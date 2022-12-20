fded7373d77068750dacb5a4b8b4d4d8b7980d8c
Paola Lara <paolara.0510@gmail.com>
2022-12-20 22:57:23 +0100

    commit

--- Water-jug.js
+++ Water-jug.js
@@ -1,72 +1,109 @@
 const xJugInput = document.querySelector('#x-jug');
 const yJugInput = document.querySelector('#y-jug');
 const zMeasureInput = document.querySelector('#z-measure');
+const solucion = document.querySelector(".solution");
 let button = document.querySelector('#bt');
 button.addEventListener('click', function() {
     fill(xJugInput.value, yJugInput.value, zMeasureInput.value);
 });
 
-function fill (X, Y, Z) { 
+function insertInHTML (txt) {
+    let h4 = document.createElement('h4');
+    h4.innerHTML = txt;
+    solucion.appendChild(h4);
+}
+
+function fill (X, Y, Z) {
     let xJug = 0;
     let yJug = 0;
     let found = false
     let count = 0;
 
-    if (X == Z) {
-        document.querySelector('h4.status').innerHTML = 'The X jug is full with the Z measure';
-    } else if (Y == Z) {
-        document.querySelector('h4.status').innerHTML = 'The Y jug is full with the Z measure';
+    if (Number(X) == Number(Z)) {
+        insertInHTML('Jug X is filled with measure Z');
+    } else if (Number(Y) == Number(Z)) {
+        insertInHTML('Jug Y is filled with measure Z');
+    } else if (Number(X) < Number(Z) && Number(Y) < Number(Z)) {
+        insertInHTML('No Solution');
+    } else if (Number(X) == Number(Y) && Number(Z) > Number(X)) {
+        insertInHTML('No Solution');
     } else {
         while (!found) {
             count++;
-            if (xJug == Z || yJug == Z) {
+
+            if(count == 10) {
+                found = false;
+                break;
+            }
+            if (xJug == Number(Z)) {
                 found = true;
+                yJug = 0;
+                break;
+            }
+            if (yJug == Number(Z)) {
+                found = true;
+                xJug = 0;
                 break;
             }
 
             if (xJug == 0) {
                 xJug = X;
+                insertInHTML(('X = '+ xJug));
                 continue;
             }
             
-            if (yJug < Y && xJug == X) {
+            if (yJug < Number(Y) && xJug == Number(X)) {
+                insertInHTML('Tranfer X to Y');
                 let diff = Y - yJug;
-                if (diff > xJug) {
-                    yJug += Number(xJug);
-                    xJug = 0;
+                if (diff > yJug) {
+                    if (Number(Y) < xJug) {
+                        yJug = Y;
+                        xJug -= Y;
+                    } else {
+                        yJug += Number(xJug);
+                        xJug = 0;
+                    }
                 } else {
                     yJug += Number(diff);
                     xJug -= Number(diff);
                 }
+                insertInHTML(('X = '+ xJug + ' Y = ' + yJug));
                 continue;
             }
 
             if (yJug == 0) {
                 yJug = Y;
+                insertInHTML(('Y = '+ yJug));
                 continue;
             }
 
-            if (xJug < X && yJug == Y) {
+            if (xJug < Number(X) && yJug == Number(Y)) {
+                insertInHTML('Tranfer Y to X');
                 let diff = X - xJug;
                 if (diff > xJug) {
-                    xJug += Number(yJug);
-                    yJug = 0;
+                    if (Number(X) < yJug) {
+                        xJug = X;
+                        yJug -= X; 
+                    } else {
+                        xJug += Number(yJug);
+                        yJug = 0;
+                    }
                 } else {
                     xJug += Number(diff);
                     yJug -= Number(diff);
                 }
+                insertInHTML(('X = '+ xJug + ' Y = ' + yJug));
                 continue;
-            }
-
-            if(count == 100) {
-                found = false;
-                break;
             }
         }   
         if (found) {
-            console.log('solution',xJug, yJug);
+            if (xJug != 0){
+                insertInHTML('Jug X is filled with measure Z');
+            } else {
+                insertInHTML('Jug Y is filled with measure Z');
+            }
         } else {
-            console.log('No hay solucion');
+            insertInHTML('No solution');
         }
     }
 }
